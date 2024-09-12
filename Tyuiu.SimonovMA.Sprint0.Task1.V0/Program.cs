c19d1c466b458fa88c269b0190cc3dfa36f8c586
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-09-12 22:20:29 +0500

    Task1

---
+++ Tyuiu.SimonovMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tyuiu.SimonovMA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Михаил";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + ", мне " + age + " лет!");
+        }
+    }
+}
