7bd8467206108b60e85570a32b7876e01b71c235
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-09-13 22:18:28 +0500

    Task5

---
+++ Tyuiu.SimonovMA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tyuiu.SimonovMA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.SimonovMA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + b = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - b = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * b = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / b = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
