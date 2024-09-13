37d607e3db601c07f67fbd7e77eff4c20dc51e46
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-09-13 20:14:26 +0500

    Task3

---
+++ Tyuiu.SimonovMA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.SimonovMA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.SimonovMA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Tyuiu.SimonovMA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.SimonovMA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
