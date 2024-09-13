10a7ccc30e370d065e6bdfc0957fc70fc2797a09
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-09-13 21:10:14 +0500

    Task4

---
+++ Tyuiu.SimonovMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tyuiu.SimonovMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.SimonovMA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Substraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
